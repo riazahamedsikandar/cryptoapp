5363cac6a0d97a4eefa50708ad331d8ac030930a
riazahamedsikandar <riazahamedsikandar@gmail.com>
2023-06-12 17:51:59 +0530

    dom

---
+++ crypto-react/src/firebase.js
@@ -0,0 +1,18 @@
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAWa_w93LS2OTB56sRu1XOAB1EfCsP-SN0",
+    authDomain: "crypto-app-4fbd0.firebaseapp.com",
+    projectId: "crypto-app-4fbd0",
+    storageBucket: "crypto-app-4fbd0.appspot.com",
+    messagingSenderId: "231308888703",
+    appId: "1:231308888703:web:0e9b21b2a78024ede5cd4a"
+  };
+
+  const app = initializeApp(firebaseConfig);
+  export const auth = getAuth(app);
+  export const db = getFirestore(app);
+  
+  export default app;
