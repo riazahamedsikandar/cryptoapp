1e54b261cfecf7a9577724421291149502a8454d
riazahamedsikandar <riazahamedsikandar@gmail.com>
2023-06-15 13:29:10 +0530

    tailwind

--- crypto-react/tailwind.config.js
+++ crypto-react/tailwind.config.js
@@ -1,9 +1,12 @@
-
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+        extend: {
+            colors: {
+                primary: '#ff0000',
+                secondary: '#00ff00',
+            },
+        },
+    },
+    plugins: [],
+};
